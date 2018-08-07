beb0fe7bb37dda9beed0f41011d880ccead8a9cf
Amman Vedi <ammanvedi@Ammans-MacBook-Pro.local>
2018-08-07 20:54:53 +0100

    update fe test cases

--- test/spec/Default.spec.js
+++ test/spec/Default.spec.js
@@ -1,8 +1,33 @@
-describe( 'Default Behaviour', function() {
+describe( 'Socket Client', function() {
     this.timeout( 10000 );
 
-    it( 'Sets default as specified by user', function( done ) {
-        expect( true ).to.equal( true );
-        done();
+    it( 'Connects to Server', function( done ) {
+
+        var client = new  IOTStateSync.SocketClient( {
+            onConnected: function() {
+                expect( true ).to.equal( true );
+                done();
+            }
+        } );
+
     } );
+} );
+
+
+describe( 'Sync State Manager', function() {
+    this.timeout( 5000 );
+
+    it( 'Registers client', function( done ) {
+
+        var manager = new IOTStateSync.ClientSyncStateManager( {
+            onConnected: function() {},
+            onRegister: function( err ) {
+
+                if( err ) {
+                    expect( true ).to.equal( false );
+                }
+                done();
+            }
+        } );
+    } ); 
 } );
